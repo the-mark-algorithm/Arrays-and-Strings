fec533f9e079ee18767c003b84a97139802ec5cf
Mark Ono <60458022+the-mark-algorithm@users.noreply.github.com>
2023-01-26 19:13:41 -0600

    solution with data structures

--- isUnique.py
+++ isUnique.py
@@ -1,20 +1,37 @@
 # Implement an algorithm to determine if a string has all unique characters.
 # What if you can't use additional data structures?
 import sys
+from collections import defaultdict
 
 class HashTable:
     keys = list()
     vals = list()
 
-    # for alphabet hash function is just its value mod 26 (size of alphabet)
+    def __init__(self, size) -> None:
+        for i in range(size):
+            self.vals.append(None)
+
+        self.size = size
 
     def add(self, key, val):
-        if key not in self.keys:
+        if key not in self.keys: # Should we care about duplicate keys?
             self.keys.append(key)
-            
+        
+        
+def isUnique(useDataStructures: bool, string: str):
+    if useDataStructures:
+        alphabet = defaultdict(int)
+        for s in string:
+            if alphabet[s] > 0: # already found an instance, not unique
+                return False
+            alphabet[s] += 1
+        return True
+    else: #todo make own hash table
+
+        pass
 
 def main():
-    
+    # argv = [ isUnique.py, useDataStrucutures (T/F), Word (String) ]
 
     pass
 
